08afa4a03c08bce4096c1381c68a000d0fbcb775
aracellyramirez513-sketch <aracellyramirez513@gmail.com>
2026-04-26 22:06:12 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,14 @@ export default function Document() {
         <meta property="og:site_name" content="Reading with Matcha" />
         <meta name="theme-color" content="#faf8f4" />
         <link rel="icon" href="/favicon.ico" />
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KN7E7ZYL2S" />
+        <script dangerouslySetInnerHTML={{__html:`
+          window.dataLayer=window.dataLayer||[];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js',new Date());
+          gtag('config','G-KN7E7ZYL2S');
+        `}}/>
       </Head>
       <body>
         <Main />
